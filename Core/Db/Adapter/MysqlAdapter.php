a46a8c872afde326caf92128484aefc4aa90c2de
Daniel Hornik <daniel.1302@gmail.com>
2017-08-29 18:45:08 +0200

    Add init command after mysql connect

--- Core/Db/Adapter/MysqlAdapter.php
+++ Core/Db/Adapter/MysqlAdapter.php
@@ -8,6 +8,7 @@ class MysqlAdapter extends PDO implements DatabaseAdapter
 {
     public function __construct($host, $user, $pass, $dbName)
     {
-        parent::__construct('mysql:dbname='.$dbName.';host='.$host, $user, $pass);
+        parent::__construct('mysql:dbname='.$dbName.';host='.$host, $user, $pass,
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     }
 }
